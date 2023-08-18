1772b51365bf2ac8ba8f1287e76682223a0cdafd
Yiğit TİLKİ <bestnemesis1234@gmail.com>
2023-08-18 07:54:07 +0300

    eski haline döndürüldü

---
+++ lib/pages/login_page_s.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:sanofi_main/pages/login_page_t.dart';
+
+import 'package:sanofi_main/pages/student_page.dart';
+
+class LoginPageS extends StatefulWidget {
+  const LoginPageS({super.key});
+
+  @override
+  State<LoginPageS> createState() => _LoginPageSState();
+}
+
+class _LoginPageSState extends State<LoginPageS> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      body: Center(
+        child: Column(
+          children: [
+            const Expanded(
+              child: SizedBox(
+                height: 119,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 16.0, bottom: 8),
+              child: SizedBox(
+                height: 51.56,
+                width: 200,
+                child: Image.asset("assets/SNY_BIG.png"),
+              ),
+            ),
+            const Text("Eğitim Yoklama Sistemi"),
+            const Expanded(
+              child: SizedBox(
+                height: 227,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 25.0, left: 25, top: 10),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  labelText: "Ad-Soyad",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 35,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 25.0, left: 25, top: 10),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  labelText: "Sicil No",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            SizedBox(
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    FocusManager.instance.primaryFocus?.unfocus();
+                  });
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => const StudentPage(),
+                    ),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.purple,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                    minimumSize: const Size(100, 35)),
+                child: const Text(
+                  "Giriş",
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const LoginPageT(),
+                  ),
+                );
+              },
+              child: const Text(
+                "Eğitimci Girişi",
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+            const Expanded(
+              child: SizedBox(
+                height: 100,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/student_page.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:sanofi_main/generate_qr.dart';
+
+class StudentPage extends StatefulWidget {
+  const StudentPage({super.key});
+
+  @override
+  State<StudentPage> createState() => _StudentPageState();
+}
+
+class _StudentPageState extends State<StudentPage> {
+  @override
+  void initState() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: [SystemUiOverlay.bottom]);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Align(
+            alignment: Alignment.topLeft,
+            child: Container(
+              height: 139,
+              width: 256,
+              decoration: const BoxDecoration(
+                color: Color(0xff8432C5),
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(44),
+                ),
+              ),
+              child: const Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Padding(
+                      padding: EdgeInsets.only(left: 20.0),
+                      child: Text(
+                        "Yiğit Tilki",
+                        style: TextStyle(fontSize: 36, color: Colors.black),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 15.0),
+                    child: Text(
+                      "15313",
+                      style: TextStyle(fontSize: 24, color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 100,
+          ),
+          generateQR(),
+          const SizedBox(
+            height: 70,
+          ),
+          Image.asset(
+            "assets/SNY_BIG.png",
+            width: 278,
+            height: 61,
+          ),
+          const SizedBox(
+            height: 100,
+          )
+        ],
+      ),
+    );
+  }
+}
